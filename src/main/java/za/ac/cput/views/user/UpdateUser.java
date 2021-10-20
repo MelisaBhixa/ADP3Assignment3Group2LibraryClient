96b1ed0acaa62dfbd6f4d4d4ddf4d6d9343c7f17
ABurger219014868 <219014868@mycput.ac.za>
2021-10-20 23:15:10 +0200

    UpdateUser still needs work

--- src/main/java/za/ac/cput/views/user/UpdateUser.java
+++ src/main/java/za/ac/cput/views/user/UpdateUser.java
@@ -1,6 +1,9 @@
 package za.ac.cput.views.user;
 
+import okhttp3.OkHttpClient;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,9 +14,11 @@ import java.awt.event.ActionListener;
  */
 public class UpdateUser extends JFrame implements ActionListener {
     //show table of users and edit field on the same window, save button, back button (back to UserMainGUI.main)
+    private static OkHttpClient client = new OkHttpClient();
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        // Future implementation - watch this space :)
     }
 
     public static void main(String[] args) {
